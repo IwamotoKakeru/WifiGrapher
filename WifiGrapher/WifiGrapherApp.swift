38f14ecfece65a0af5c6d80fa9a00b2e331ef7f9
IwamotoKakeru <kganpipi@gmail.com>
2026-03-09 20:16:17 +0900

    Initial Commit

---
+++ WifiGrapher/WifiGrapherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WifiGrapherApp.swift
+//  WifiGrapher
+//
+//  Created by 岩本　翔 on 2026/03/09.
+//
+
+import SwiftUI
+
+@main
+struct WifiGrapherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
